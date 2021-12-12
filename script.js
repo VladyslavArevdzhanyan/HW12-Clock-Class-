f98d63997e9ebeadba3b40e9210afe7087a62ea4
Vladyslav <frontendvlados@gmail.com>
2021-12-12 11:31:06 +0200

    bug fixes

--- script.js
+++ script.js
@@ -1,34 +1,28 @@
 class Clock {
     constructor (clock) {
         this.clock = clock;
+        this.clock.addEventListener('click', () => this.clock.classList.toggle('isFull'));
     }
 
     render() {
-        this.time = new Date();
-        this.hours = (this.time.getHours() < 10) ? '0' + this.time.getHours() : this.time.getHours();
-        this.minutes = (this.time.getMinutes() < 10) ? '0' + this.time.getMinutes() : this.time.getMinutes();
-        this.seconds = (this.time.getSeconds() < 10) ? '0' + this.time.getSeconds() : this.time.getSeconds(); 
-        this.clock.innerHTML = `${this.hours}:${this.minutes}` + `<span>:${this.seconds}</span>`;
+        this.clock.innerHTML = this.getTime();
     }
 
-    clickClock() {
-        let block = document.getElementById('clockBlock');
-        block.addEventListener('click', function (event) { 
-            let target = event.target; 
-            if (target === block) {
-                function toggleSec(element) {
-                    element.classList.toggle('remove')
-                }
-                return toggleSec(block);
-            }
-        })
+    getTime() {
+        let isFullTime = this.clock.classList.contains('isFull');
+        const time = new Date();
+        const hours = (time.getHours() < 10) ? '0' + time.getHours() : time.getHours();
+        const minutes = (time.getMinutes() < 10) ? '0' + time.getMinutes() : time.getMinutes();
+        const seconds = (time.getSeconds() < 10) ? '0' + time.getSeconds() : time.getSeconds();
+        
+        if(isFullTime) {
+            return `${hours}:${minutes}:${seconds}`
+        } else {
+            return `${hours}:${minutes}`;
+        }
     }
 }
 
 let time = document.getElementById('clockBlock');
-
 let clocks = new Clock(time);
-clocks.render();
-clocks.clickClock();
-
-setInterval(() => {clocks.render()}, 250);
+setInterval(() => clocks.render(), 250);
